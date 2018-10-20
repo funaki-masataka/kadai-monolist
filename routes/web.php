00c64ad75798e73fd223c50150088374103d9451
masataka <masataka04748671@yahoo.ne.jp>
2018-10-20 23:10:41 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 Route::get('/', 'WelcomeController@index');
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
