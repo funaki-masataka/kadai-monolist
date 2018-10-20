6bf0ed7cea6789d3da656c1a71eb5af9485b0551
masataka <masataka04748671@yahoo.ne.jp>
2018-10-20 23:45:44 +0000

    rakuten apo

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create']]);
+});
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
