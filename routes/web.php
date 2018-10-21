75e9949112bbd73a8a3078e0d90cad67e3b44303
masataka <masataka04748671@yahoo.ne.jp>
2018-10-21 00:08:24 +0000

    want

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,13 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create']]);
 });
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
+    Route::post('want', 'ItemUserController@want')->name('item_user.want');
+    Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
+    Route::resource('users', 'UsersController', ['only' => ['show']]);
+});
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
